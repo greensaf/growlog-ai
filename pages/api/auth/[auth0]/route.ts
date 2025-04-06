f264a194238ce3241da70383e8658a5bb180d447
AlexanderSafiulin <115018603+greensaf@users.noreply.github.com>
2025-04-06 21:02:32 +0700

    fdsfas
    d

--- pages/api/auth/[auth0]/route.ts
+++ pages/api/auth/[auth0]/route.ts
@@ -1,12 +1,24 @@
 import {
-  handleAuth,
   handleLogin,
   handleLogout,
   handleCallback,
 } from '@auth0/nextjs-auth0/edge';
 
-export const GET = handleAuth({
-  login: handleLogin,
-  logout: handleLogout,
-  callback: handleCallback,
-});
+
+export const GET = async (req: Request) => {
+  const { pathname } = new URL(req.url);
+
+  if (pathname.includes('/login')) {
+    return handleLogin(req);
+  }
+
+  if (pathname.includes('/logout')) {
+    return handleLogout(req);
+  }
+
+  if (pathname.includes('/callback')) {
+    return handleCallback(req);
+  }
+
+  return new Response('Not found', { status: 404 });
+};
