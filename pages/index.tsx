5a65e25e43c7d69882208ff05932611098e40cca
AlexanderSafiulin <115018603+greensaf@users.noreply.github.com>
2025-04-09 10:12:40 +0700

    s
    s

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div className='text-center mt-20 text-2xl font-bold'>
+      🌿 Growlog AI запущен успешно!
+    </div>
+  );
+}
