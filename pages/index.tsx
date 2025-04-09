459d9f25a37ed8eb1b1c297d38d03ed32002cd48
AlexanderSafiulin <115018603+greensaf@users.noreply.github.com>
2025-04-09 11:06:06 +0700

    s
    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 // pages/index.tsx
-export default function Home() {
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+
+function Home({ user }: any) {
   return (
     <div className='text-center mt-20 text-2xl font-bold'>
+      👤 Привет, {user.name}!
+      <br />
       🌿 Growlog AI запущен успешно!
+      <br />
+      <a
+        href='/api/auth/logout'
+        className='mt-4 inline-block text-blue-500 underline'
+      >
+        Выйти
+      </a>
     </div>
   );
 }
+
+export const getServerSideProps = withPageAuthRequired();
+
+export default Home;
