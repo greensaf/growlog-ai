bb1ecf8eccd8c8c70849f6a0c30f1bfdf573d00f
AlexanderSafiulin <115018603+greensaf@users.noreply.github.com>
2025-04-10 18:56:25 +0700

    ss
    ss

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,13 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta name='theme-color' content='#22c55e' />
         <link rel='manifest' href='/manifest.json' />
         <link rel='icon' href='/favicon.ico' />
         <link rel='apple-touch-icon' href='/logo.png' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-title' content='Growlog.AI' />
+        <meta name='theme-color' content='#000000' />
+        <link rel='apple-touch-icon' href='/icon-192.png' />
       </Head>
       <body>
         <Main />
