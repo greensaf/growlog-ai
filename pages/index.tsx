9dab16edf326805de61f0427b4b3877c443125d8
Alex <115018603+greensaf@users.noreply.github.com>
2025-04-14 13:40:05 +0700

    d
    d

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,8 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-background">
       <ChatWidget />
-      <GrowlogApp />;
     </main>
   );
 }
+
+//      <GrowlogApp />;
