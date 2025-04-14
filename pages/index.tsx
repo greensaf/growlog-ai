0cc367b73ab2073183c7e722a5a1759dd398a588
Alex <115018603+greensaf@users.noreply.github.com>
2025-04-14 15:53:05 +0700

    d
    w

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 // pages/index.tsx
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import GrowlogApp from '@/components/GrowlogApp';
-import { ChatWidget } from "@/components/ChatWidget";
 
 export const getServerSideProps = withPageAuthRequired();
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-background">
-      <ChatWidget />
+      <GrowlogApp />
     </main>
   );
 }
 
-//      <GrowlogApp />;
+//
