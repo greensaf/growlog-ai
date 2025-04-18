3faf0d4c0f5afffdaea3960974309f424dc842d1
AlexanderSafiulin <115018603+greensaf@users.noreply.github.com>
2025-04-18 19:28:50 +0700

    e
    w

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </UserProvider>
   );
